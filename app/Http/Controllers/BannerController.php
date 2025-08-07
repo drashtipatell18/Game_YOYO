30f3f15cf650106b0af2789f95b7120ef658152e
Dasu <dasuuu1812@gmail.com>
2025-08-07 12:34:54 +0530

    add model call

--- app/Http/Controllers/BannerController.php
+++ app/Http/Controllers/BannerController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Banner;
 class BannerController extends Controller
 {
      public function bannerCreate(Request $request)
